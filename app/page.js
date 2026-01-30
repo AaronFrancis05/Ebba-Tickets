79b504b8d5c65ae3377d5d038fce1479f5aec6b8
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-01-30 09:27:12 +0300

    vercelworking

--- app/page.js
+++ app/page.js
@@ -1,18 +1,16 @@
-'use client';
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+
 
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { use } from "react";
 
 export default function Home() {
-  const pathname = usePathname();
+  
   return (
     <main>
       Home
 
-      <Button className={cn("bg-blue-600", pathname === "/" ? "text-white" : "text-gray-400")}>Click Me</Button>
+     
        </main>
   );
 }
