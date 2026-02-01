1348af635ef584bca03052790c533cf0f3c66146
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-02-02 01:32:53 +0300

    Onboarding Error Fix

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import { Button } from "../../../components/ui/button";
 import { cn } from "@/lib/utils";
 
 export default function Home() {
--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,6 +1,4 @@
 
-
-
 export default function RootLayout({ children }) {
   return (
    
