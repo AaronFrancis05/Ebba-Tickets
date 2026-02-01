e646d78cdd400f6891bd87a02465ee271958361e
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-02-02 02:15:21 +0300

    Onbaording Fix 3

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Nav from "@/components/Nav";
+import Nav from "@/components/nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
