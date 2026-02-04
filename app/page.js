911ada70078590e633738390996464c58ef17d11
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-02-04 12:50:40 +0300

    Clerk Billing

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { partners } from "@/data/constants";
+import { PricingTable } from "@clerk/nextjs";
 import Image from "next/image"; 
 
 export default function Home() {
@@ -6,6 +8,9 @@ export default function Home() {
 
   return (
     <main className="flex flex-col justify-center items-center p-5">
+        <div className="rounded-4xl p-4 bg-purple-600  outline-2 outline-purple-800 text-white font-semibold max-w-lg w-full flex justify-center items-center">
+        <p className="font-bold">Trusted by 1000+ event organizers in Uganda</p>
+        </div>
       <section className="flex flex-col  pt-4 max-sm:mx-4 max-w-4xl mx-auto">
           <h2 className="font-bold text-6xl font-serif ">
             Events you can trust,
@@ -23,12 +28,26 @@ export default function Home() {
                   Learn</Button>    
             </div>
             <div className="pt-5 w-full">
-              <Image className="object-fit rounded-3xl" src={'/event2.jpeg'} height={300} width={850} alt='ticket-image'/>
+              <Image className="object-fit rounded-3xl" src={'/images/event2.jpeg'} height={300} width={850} alt='ticket-image'/>
             </div>
       </section>
 
-      <section className="flex justify-center items-center pt-8 max-sm:mx-4">
+      <section>
+      <div className="flex justify-center items-center pt-8 max-sm:mx-4">
             <h5 className="font-bold font-sans text-2xl "> Trusted by Kampala's best event organizers.</h5>
+      </div>
+      <div className="mt-5 flex gap-5 bg-amber-700 rounded-4xl p-3">
+        {partners.map((partner)=>
+        <div key={partner.id} className="flex justify-center items-center">
+          <Image src={partner.icon} width={70} height={70} alt={partner.name}/>
+          <p className="pl-3 font-bold text-white">{partner.name}</p>
+        </div>)}
+      </div>
+      </section>
+
+      <section className="w-full max-w-6xl pt-16 flex">
+
+        <PricingTable />
       </section>
       
     </main>
---
+++ data/constants.js
@@ -0,0 +1,12 @@
+import badge from "@/public/icons/badge.png"
+import brand from "@/public/icons/brand.png"
+import loyalty from "@/public/icons/loyalty-program.png"
+
+export  const partners=[
+  {id:1, name:'badge', icon: badge},
+  {id:2, name:'brand', icon: brand},
+  {id:3, name:'loyalty', icon: loyalty},
+  {id:4, name:'badge', icon: badge},
+  {id:5, name:'brand', icon: brand},
+  {id:6, name:'loyalty', icon: loyalty},
+]
